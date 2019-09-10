08640482f4d9a9acfacc652bb717ff394c85abd4
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2019-09-10 13:02:49 +0200

    feat: extracted App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { EditableTimebox, TimeboxList } from "./remaining-component.js";
+
+function App() {
+    return (
+        <div className="App">
+            <TimeboxList />
+            <EditableTimebox />
+        </div>
+    );
+}
+
+export default App;
