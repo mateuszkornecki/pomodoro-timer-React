713b9e0ede72a749193c75654b13063ff4c8f5a5
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2019-09-11 09:25:59 +0200

    Updates

---
+++ precache-manifest.a4e636239f1d42a7bc60649d802a55e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8fa32a209a2374b043e9accc5394176",
+    "url": "/pomodoro-timer-React/index.html"
+  },
+  {
+    "revision": "ecbf89a5c05a7a0783bc",
+    "url": "/pomodoro-timer-React/static/css/main.5fcf95cf.chunk.css"
+  },
+  {
+    "revision": "9178816e9e9e658ac7be",
+    "url": "/pomodoro-timer-React/static/js/2.754eb448.chunk.js"
+  },
+  {
+    "revision": "ecbf89a5c05a7a0783bc",
+    "url": "/pomodoro-timer-React/static/js/main.f274b949.chunk.js"
+  },
+  {
+    "revision": "374493d143574b691b63",
+    "url": "/pomodoro-timer-React/static/js/runtime~main.16ff725a.js"
+  }
+]);
