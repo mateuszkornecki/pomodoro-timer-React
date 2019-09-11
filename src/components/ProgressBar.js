38b0b558c9025bbc6c72dc7ec6c4bbfe7e91a5cc
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2019-09-11 12:39:02 +0200

    feat: added modificator --red to progress__bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-function ProgressBar({ className = "", percent, trackRemaining }) {
+function ProgressBar({ className, percent, trackRemaining }) {
     const ProgressBarClasses =
         trackRemaining === true
-            ? `ProgressBar ProgressBar--reverse ${className}`
-            : `ProgressBar ${className}`;
+            ? `progress progress--reverse ${className}`
+            : `progress ${className}`;
     const ProgressBarWidth =
-        trackRemaining === true ? `calc(100% - ${percent}%)` : `${percent}%`;
+        trackRemaining === true ? `calc(100 % - ${percent} %)` : `${percent}% `;
 
     return (
         <div className={ProgressBarClasses}>
-            <div style={{ width: `${ProgressBarWidth}` }}></div>
+            <div className="progress__bar progress__bar--red" style={{ width: `${ProgressBarWidth} ` }}></div>
         </div>
     );
 }
