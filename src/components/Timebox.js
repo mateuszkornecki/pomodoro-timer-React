7b260fc8931e0242a7770072fa91938930807cf6
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-09-20 10:04:41 +0200

    feat: added new error when given taskTime is <= 0

--- src/components/Timebox.js
+++ src/components/Timebox.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 function Timebox({ title, taskTime, onDelete, onEdit, index, onChange }) {
+    if (taskTime <= 0) {
+        throw new Error("Czas zadania musi być większy niż zero");
+    }
+
     return (
         <div className="Timebox">
             <h3>
