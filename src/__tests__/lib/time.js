473011732d28063327af2ace58e8d3f7ff57ab64
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-09-22 22:47:47 +0200

    refactor: updated tests descriptions

--- src/__tests__/lib/time.js
+++ src/__tests__/lib/time.js
@@ -1,31 +1,35 @@
 import { reworkTime } from "../../lib/time";
 
-test("check if function reworkTime works for 0 seconds", () => {
-    expect(
-        reworkTime(0)
-    ).toEqual([0, 0, 0, 0])
-});
+describe("reworkTime", () => {
 
-test("check if function reworkTime works for 0.5 seconds", () => {
-    expect(
-        reworkTime(0.5)
-    ).toEqual([0, 0, 0, 500])
-});
+    test("returns 0 hours, 0 minutes, 0 seconds, 0 ms for 0 seconds duration", () => {
+        expect(
+            reworkTime(0)
+        ).toEqual([0, 0, 0, 0])
+    });
 
-test("check if function reworkTime works for 30 seconds", () => {
-    expect(
-        reworkTime(30)
-    ).toEqual([0, 0, 30, 0])
-});
+    test("returns 0 hours, 0 minutes, 0 seconds, 500 ms for 0.5 seconds duration", () => {
+        expect(
+            reworkTime(0.5)
+        ).toEqual([0, 0, 0, 500])
+    });
 
-test("check if function reworkTime works for 60 seconds", () => {
-    expect(
-        reworkTime(60)
-    ).toEqual([0, 1, 0, 0])
-});
+    test("returns 0 hours, 0 minutes, 30 seconds, 0 ms for 30 seconds duration", () => {
+        expect(
+            reworkTime(30)
+        ).toEqual([0, 0, 30, 0])
+    });
 
-test("check if function reworkTime works for 3600 seconds", () => {
-    expect(
-        reworkTime(3600)
-    ).toEqual([1, 0, 0, 0])
-});
+    test("returns 0 hours, 1 minutes, 0 seconds, 0 ms for 60 seconds duration", () => {
+        expect(
+            reworkTime(60)
+        ).toEqual([0, 1, 0, 0])
+    });
+
+    test("returns 1 hours, 0 minutes, 0 seconds, 0 ms for 3600 seconds duration", () => {
+        expect(
+            reworkTime(3600)
+        ).toEqual([1, 0, 0, 0])
+    });
+
+})
