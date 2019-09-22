bfa9ebe68f3f604d4a65a6e3e64674c89c0f4380
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-09-22 21:09:17 +0200

    feat: created some tests to check if reworkTime function works properly

--- src/__tests__/lib/time.js
+++ src/__tests__/lib/time.js
@@ -1,3 +1,31 @@
-test("check if true is false", () => {
-    expect(true).toBe(false);
+import { reworkTime } from "../../lib/time";
+
+test("check if function reworkTime works for 0 seconds", () => {
+    expect(
+        reworkTime(0)
+    ).toEqual([0, 0, 0, 0])
+});
+
+test("check if function reworkTime works for 0.5 seconds", () => {
+    expect(
+        reworkTime(0.5)
+    ).toEqual([0, 0, 0, 500])
+});
+
+test("check if function reworkTime works for 30 seconds", () => {
+    expect(
+        reworkTime(30)
+    ).toEqual([0, 0, 30, 0])
+});
+
+test("check if function reworkTime works for 60 seconds", () => {
+    expect(
+        reworkTime(60)
+    ).toEqual([0, 1, 0, 0])
+});
+
+test("check if function reworkTime works for 3600 seconds", () => {
+    expect(
+        reworkTime(3600)
+    ).toEqual([1, 0, 0, 0])
 });
