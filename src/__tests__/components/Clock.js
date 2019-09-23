7acceaaf12bce20feb0f22d74545160a7b4fc71c
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-09-23 16:43:24 +0200

    refactor: cleaned up code

--- src/__tests__/components/Clock.js
+++ src/__tests__/components/Clock.js
@@ -39,34 +39,28 @@ describe('<Clock/> with react-test-renderer', () => {
             <Clock hours={1} minutes={20} seconds={42} miliseconds={311} />
         );
     });
-    describe('tests for rendering', () => {
-        it('render h2 when given hours, minutes , seconds and miliseconds', () => {
-            expect(clockRenderer.toJSON().type).toEqual("h2");
+    //! PYTANIE NA LIVE, dlaczego nie zrobić po prostu ...props.className żeby dostać się do konkretnego klucza?
+    describe('when given hours, minutes, seconds, miliseconds', () => {
+        it('renders properly', () => {
+            expect(clockRenderer.toJSON()).toMatchSnapshot();
         });
-        //! PYTANIE NA LIVE, dlaczego nie zrobić po prostu ...props.className żeby dostać się do konkretnego klucza?
-        it('has className ="clock" when given hours, minutes, seconds, and miliseconds', () => {
+        it('has className ="clock" ', () => {
             expect(clockRenderer.toJSON().props.className).toMatch(/clock/);
         });
-        describe('render time properly when given hours, minutes, seconds and miliseconds', () => {
-
-            it('render hours', () => {
-                expect(clockRenderer.toTree().props.hours).toBe(1);
-            });
-            it('render minutes', () => {
-                expect(clockRenderer.toTree().props.minutes).toBe(20);
-            });
-            it('render seconds', () => {
-                expect(clockRenderer.toTree().props.seconds).toBe(42);
-            });
-            it('render miliseconds', () => {
-                expect(clockRenderer.toTree().props.miliseconds).toBe(311);
-            });
+        it('renders h2', () => {
+            expect(clockRenderer.toJSON().type).toEqual("h2");
         });
-
-    });
-    describe('sets correctly default props values', () => {
-        it('sets Classname to empty string and hours,minutes, seconds, miliseconds to 0 if not given anything else', () => {
-            expect(<Clock />).toEqual(<Clock className="" hours={0} minutes={0} seconds={0} miliseconds={0} />);
+        it('renders hours', () => {
+            expect(clockRenderer.toTree().props.hours).toBe(1);
+        });
+        it('renders minutes', () => {
+            expect(clockRenderer.toTree().props.minutes).toBe(20);
+        });
+        it('renders seconds', () => {
+            expect(clockRenderer.toTree().props.seconds).toBe(42);
+        });
+        it('renders miliseconds', () => {
+            expect(clockRenderer.toTree().props.miliseconds).toBe(311);
         });
     });
 });
