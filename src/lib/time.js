a8f7d180236c07dc1db33c058c89cb68aae35220
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-09-23 09:38:29 +0200

    fix: fixed reworkTime bug for duration higher than 3600 seconds

--- src/lib/time.js
+++ src/lib/time.js
@@ -1,7 +1,8 @@
 const reworkTime = value => {
     const fullSec = Math.floor(value);
     const hours = Math.floor(value / 3600);
-    const minutes = Math.floor(value / 60) === 60 ? 0 : Math.floor(value / 60);
+    const minutes = Math.floor(value / 60) >= 60 ? Math.floor(value / 60) - 60 : Math.floor(value / 60);
+    // const minutes = Math.floor(value / 60) === 60 ? 0 : Math.floor(value / 60)
     const seconds = Math.floor(value % 60);
     //to prevent counting after passing 0
     const miliseconds = fullSec >= 0 ? Math.floor((value - fullSec) * 1000) : 0;
