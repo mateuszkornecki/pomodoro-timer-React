0910b740109588237dae8b239dd1f46ccb5cadd1
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-10-06 16:46:53 +0200

    feat: updated add, replace, remove Timebox methods to work on json-server (db.json)

--- src/api/FetchTimeboxesApi.js
+++ src/api/FetchTimeboxesApi.js
@@ -2,20 +2,59 @@
 const FetchTimeboxesAPI = {
     getAllTimeboxes: async function () {
         const response = await fetch("http://localhost:4000/timeboxes");
+        if (!response.ok) {
+            throw new Error("Something went wrong!...");
+        }
         const timeboxes = await response.json();
         return timeboxes;
     },
     addTimebox: async function (timeboxToAdd) {
-
-        return {};
+        const response = await fetch("http://localhost:4000/timeboxes", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(timeboxToAdd)
+        });
+        if (!response.ok) {
+            throw new Error("Something went wrong!...");
+        }
+        const addedTimebox = await response.json();
+        return addedTimebox;
     },
     replaceTimebox: async function (timeboxToReplace) {
-
-        return {};
+        if (!timeboxToReplace.id) {
+            throw new Error("Timebox has to have an id to be updated");
+        }
+        const response = await fetch(`http://localhost:4000/timeboxes/${timeboxToReplace.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(timeboxToReplace)
+        });
+        if (!response.ok) {
+            throw new Error("Something went wrong!...");
+        }
+        const replacedTimebox = await response.json();
+        return replacedTimebox;
     },
     removeTimebox: async function (timeboxToRemove) {
-
-        return {};
+        if (!timeboxToRemove.id) {
+            throw new Error("Timebox has to have an id to be updated");
+        }
+        const response = await fetch(`http://localhost:4000/timeboxes/${timeboxToRemove.id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(timeboxToRemove)
+        });
+        if (!response.ok) {
+            throw new Error("Something went wrong!...");
+        }
+        const removedTimebox = await response.json();
+        return removedTimebox;
     }
 
 }
