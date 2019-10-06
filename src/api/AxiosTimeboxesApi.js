9f4b6aee290dd2269ec84bf2a6b6c103225ea9c8
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-10-06 19:37:50 +0200

    feat: created AxiosTimeboxesApi.js

---
+++ src/api/AxiosTimeboxesApi.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+const BASE_URL = "http://localhost:4000/timeboxes";
+
+const AxiosTimeboxesAPI = {
+    getAllTimeboxes: async function () {
+        const response = await axios.get(BASE_URL);
+        const timeboxes = await response.data;
+        return timeboxes;
+    },
+    addTimebox: async function (timeboxToAdd) {
+        const response = await axios.post(BASE_URL, timeboxToAdd);
+        const addedTimebox = await response.data;
+        return addedTimebox;
+    },
+    replaceTimebox: async function (timeboxToReplace) {
+        if (!timeboxToReplace.id) {
+            throw new Error("Timebox has to have an id to be updated");
+        }
+        const response = await axios.put(`${BASE_URL}/${timeboxToReplace.id}`, timeboxToReplace);
+        const replacedTimebox = await response.data;
+        return replacedTimebox;
+    },
+    removeTimebox: async function (timeboxToRemove) {
+        if (!timeboxToRemove.id) {
+            throw new Error("Timebox has to have an id to be updated");
+        }
+        const response = await axios.delete(`${BASE_URL}/${timeboxToRemove.id}`);
+        //! no need to return anything, if Promise wont throw error it means that timebox was removed
+    }
+}
+
+export default AxiosTimeboxesAPI;
