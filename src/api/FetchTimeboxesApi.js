f63d71036ebb729a2d927d8b90c077cd6fa18a78
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-10-06 17:46:53 +0200

    feat: refactor: simplified FetchTimeboxesAPI methods

--- src/api/FetchTimeboxesApi.js
+++ src/api/FetchTimeboxesApi.js
@@ -1,62 +1,40 @@
-
+const BASE_URL = "http://localhost:4000/timeboxes";
 const FetchTimeboxesAPI = {
     getAllTimeboxes: async function () {
-        const response = await fetch("http://localhost:4000/timeboxes");
-        if (!response.ok) {
-            throw new Error("Something went wrong!...");
-        }
-        const timeboxes = await response.json();
+        const timeboxes = makeRequest(BASE_URL, "GET");
         return timeboxes;
     },
     addTimebox: async function (timeboxToAdd) {
-        const response = await fetch("http://localhost:4000/timeboxes", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(timeboxToAdd)
-        });
-        if (!response.ok) {
-            throw new Error("Something went wrong!...");
-        }
-        const addedTimebox = await response.json();
+
+        const addedTimebox = makeRequest(BASE_URL, "POST", timeboxToAdd);
         return addedTimebox;
     },
     replaceTimebox: async function (timeboxToReplace) {
-        if (!timeboxToReplace.id) {
-            throw new Error("Timebox has to have an id to be updated");
-        }
-        const response = await fetch(`http://localhost:4000/timeboxes/${timeboxToReplace.id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(timeboxToReplace)
-        });
-        if (!response.ok) {
-            throw new Error("Something went wrong!...");
-        }
-        const replacedTimebox = await response.json();
+        const replacedTimebox = makeRequest(`${BASE_URL}/${timeboxToReplace.id}`, "PUT", timeboxToReplace);
         return replacedTimebox;
     },
     removeTimebox: async function (timeboxToRemove) {
         if (!timeboxToRemove.id) {
             throw new Error("Timebox has to have an id to be updated");
         }
-        const response = await fetch(`http://localhost:4000/timeboxes/${timeboxToRemove.id}`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(timeboxToRemove)
-        });
-        if (!response.ok) {
-            throw new Error("Something went wrong!...");
-        }
-        const removedTimebox = await response.json();
+        const removedTimebox = makeRequest(`${BASE_URL}/${timeboxToRemove.id}`, "DELETE", timeboxToRemove);
         return removedTimebox;
     }
-
 }
 
 export default FetchTimeboxesAPI;
+
+async function makeRequest(url, method, body) {
+    const jsonBody = body ? JSON.stringify(body) : undefined;
+    const response = await fetch(url, {
+        method: method,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: jsonBody
+    })
+    if (!response.ok) {
+        throw new Error("Something went wrong!...");
+    }
+    return await response.json();
+}
