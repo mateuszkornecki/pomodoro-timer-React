1a2c306529a9523cf3f61d405c116faf4f2fd1d0
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-10-07 17:00:32 +0200

    feat: converted App to class component

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,24 @@ import EditableTimebox from "./EditableTimebox";
 import TimeboxList from "./TimeboxList";
 import ErrorBoundary from "./ErrorBoundary";
 
-
-function App() {
-    return (
-        <ErrorBoundary message="wystąpił błąd całej aplikacji">
-            <div className="App">
-                <header className="header">
-                    Witaj test01@gmail.com.
+class App extends React.Component {
+    render() {
+        return (
+            <ErrorBoundary message="wystąpił błąd całej aplikacji" >
+                <div className="App">
+                    <header className="header">
+                        Witaj test01@gmail.com.
                 <a className="header__logout-link" href="#">Wyloguj</a>
 
-                </header>
-                <TimeboxList />
-                <ErrorBoundary message="wystąpił błąd komponentu EditableTimebox">
-                    <EditableTimebox />
-                </ErrorBoundary>
-            </div>
-        </ErrorBoundary>
-    );
+                    </header>
+                    <TimeboxList />
+                    <ErrorBoundary message="wystąpił błąd komponentu EditableTimebox">
+                        <EditableTimebox />
+                    </ErrorBoundary>
+                </div>
+            </ErrorBoundary>
+        );
+    }
 }
 
 export default App;
