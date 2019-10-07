979f02b1d51ae0fecfbe0ccfac0e92d27175ef7b
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-10-07 17:43:40 +0200

    feat: created LoginForm component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.loginForm = React.createRef();
+    }
+
+    handleSubmit = e => {
+        const { onLoginAttempt } = this.props;
+        e.preventDefault();
+        onLoginAttempt({
+            email: this.loginForm.current.children.emailInput.value,
+            password: this.loginForm.current.children.passwordInput.value
+        });
+        this.loginForm.current.children.emailInput.value = "";
+        this.loginForm.current.children.passwordInput.value = "";
+    };
+
+    render() {
+        return (
+            <form
+                ref={this.loginForm}
+                onSubmit={this.handleSubmit}
+                className="LoginForm"
+            >
+                <div className="LoginForm__error-message">
+                    {this.props.errorMessage}
+                </div>
+                <label htmlFor="emailInputt">Email: </label>
+                <input id="emailInput" type="email" />
+                <br />
+                <label htmlFor="passwordInput">Password: </label>
+                <input id="passwordInput" type="password" />
+                <br />
+                <button>Zaloguuj</button>
+            </form>
+        );
+    }
+}
+
+export default LoginForm;
