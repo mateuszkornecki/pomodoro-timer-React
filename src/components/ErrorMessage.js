7ae49d20b7da691a71d55617c4817ecfdac9f09d
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-10-07 17:43:17 +0200

    fix: changed class for className

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function ErrorMessage({ message, children, hasError }) {
     return (
-        <div>{hasError ? <div class="ErrorMessage">{message}</div> : children}</div>
+        <div>{hasError ? <div className="ErrorMessage">{message}</div> : children}</div>
     )
 }
 
