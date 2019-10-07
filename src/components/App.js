ca8f122f5fc2b829dc36db5458316aadefa42545
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-10-07 17:44:58 +0200

    feat: added few methods to handle LoginForm

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,53 @@ import React from "react";
 import EditableTimebox from "./EditableTimebox";
 import TimeboxList from "./TimeboxList";
 import ErrorBoundary from "./ErrorBoundary";
-
+import LoginForm from "./LoginForm";
 class App extends React.Component {
-    render() {
-        return (
-            <ErrorBoundary message="wystąpił błąd całej aplikacji" >
-                <div className="App">
-                    <header className="header">
-                        Witaj test01@gmail.com.
-                <a className="header__logout-link" href="#">Wyloguj</a>
 
-                    </header>
-                    <TimeboxList />
-                    <ErrorBoundary message="wystąpił błąd komponentu EditableTimebox">
-                        <EditableTimebox />
-                    </ErrorBoundary>
-                </div>
-            </ErrorBoundary>
-        );
-    }
+
+	getUserEmail = () => {
+		return "test01@gmail.com";
+	}
+
+	handleLogout = () => {
+		console.log("handling logout");
+	}
+
+	handleLoginAttempt = (credentials) => {
+		console.log("handling login attempt", credentials);
+	}
+
+	isUserLoggedIn = () => {
+		return false;
+	}
+
+	render() {
+		return (
+			<ErrorBoundary message="wystąpił błąd całej aplikacji" >
+				<div className="App">
+					{
+						this.isUserLoggedIn()
+							?
+							<>
+								<header className="header">
+									Witaj test01@gmail.com.
+                                    <a className="header__logout-link" href="#">
+										Wyloguj
+                                    </a>
+								</header>
+							</>
+							: <LoginForm
+								errorMessage="Nie udało się zalogować!"
+								onLoginAttempt={this.handleLoginAttempt} />
+					}
+					<TimeboxList />
+					<ErrorBoundary message="wystąpił błąd komponentu EditableTimebox">
+						<EditableTimebox />
+					</ErrorBoundary>
+				</div>
+			</ErrorBoundary>
+		);
+	}
 }
 
 export default App;
